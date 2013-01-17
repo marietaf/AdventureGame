d6678ab759236695fc27edaf672cccf02375d8c4
Marieta F <marietafarova@gmail.com>
2013-01-16 19:28:41 -0500

    Added temp exit button in Main Menu

--- SummativeProject/src/summativeproject/MainMenuState.java
+++ SummativeProject/src/summativeproject/MainMenuState.java
@@ -34,24 +34,31 @@ public class MainMenuState extends BasicGameState {
 
     public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
         playGame = new Image("data/playnowimage.jpg");
-        //exitGame = new Image("data/");
+        exitGame = new Image("data/exitimage.png");
     }
 
     public void render(GameContainer container, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         grphcs.drawString("Adventure Game", 180, 50);
         grphcs.drawString("by Aron, Kelvin, Marieta", 130, 70);
         playGame.draw(150, 120);
+        exitGame.draw((192), (340));
     }
 
     public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
         int posX = Mouse.getX();
         int posY = Mouse.getY();
 
-        //playGame button clicking
+        //playGame button
         //coordinates start at bottom left
         if((posX > 150 && posX < 335) && (posY > 200 && posY < 350)){
             if(Mouse.isButtonDown(0)){
                 sbg.enterState(1);
+            }
+        }
+        //exitGame button
+        if((posX > 192 && posX < 292) && (posY > 40 && posY < 140)){
+            if(Mouse.isButtonDown(0)){
+                System.exit(0);
             }
         }
     }
